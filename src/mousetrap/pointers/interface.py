5cbee3178178c5a2e1a76d0ae804b82803d32906
Stoney Jackson <dr.stoney@gmail.com>
2014-06-12 13:34:42 -0400

    Replace Exception with NotImplementedError in interface.

--- src/mousetrap/pointers/interface.py
+++ src/mousetrap/pointers/interface.py
@@ -6,8 +6,8 @@ that inherits mousetrap.pointers.interface.Pointer.
 
 class Pointer(object):
     def update_image(self, image):
-        raise Exception('Unimplemented method add_image().')
+        raise NotImplementedError('Must implement.')
 
     def get_new_position(self):
         '''Returns new location (x, y) or None if no change.'''
-        raise Exception('Unimplemented method get_position().')
+        raise NotImplementedError('Must implement.')

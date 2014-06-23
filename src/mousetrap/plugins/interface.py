e1260cfc55aef8d8570b6fbf900e17bbe3c35ab1
Stoney Jackson <dr.stoney@gmail.com>
2014-06-23 14:54:44 -0400

    Update Plugin interface reflect that constructor must take a Config.

--- src/mousetrap/plugins/interface.py
+++ src/mousetrap/plugins/interface.py
@@ -1,4 +1,9 @@
 class Plugin(object):
+    def __init__(self, config):
+        '''Override to initialize and configure yourself.
+        (Do not call parent/this constructor.)'''
+        raise NotImplementedError('Must implement.')
+
     def run(self, app):
         '''Called each pass of the loop.'''
         raise NotImplementedError('Must implement.')

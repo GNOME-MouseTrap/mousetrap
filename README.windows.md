7c58b0342b061d4e0fc601c9c541df5c9a3f6751
Stoney Jackson <dr.stoney@gmail.com>
2014-07-21 23:55:20 -0400

    Initial attempt to support Windows.
    Asking interested students or faculty to natively install Fedora or Ubuntu
    is a tall order. Plus it would be nice to reach a wider audience.
    
    I got this working under Windows 8 on a Lenova Yoga laptop.  Prerequisites
    and instructions are in README.windows.md.
    
    To make it work, I had to make the following changes:
    
    * Disabled gettext. For whatever reason, it could not find the
      .mo file. It was generated correctly, and it existed (programmatically
      checked in i18n.py) but gettext still claimed it couldn't find it.
    
    * Downgraded version numbers for automake and autoconf. We might want to
      downgrade these in the mainline too.
    
    * Removed USE_COMMON_DOC_BUILD flag in autogen.sh which is checked in
      gnome-autogen.sh. This was triggering gnome-autogen.sh to check for a
      full gnome-common enviornment, which I don't have under Windows.
      Turns out that USE_COMMON_DOC_BUILD is obsolete anyway
      (https://bugzilla.gnome.org/show_bug.cgi?id=729931). It should be
      removed in the mainline too.
    
    * Added gnome-autogen.sh. This ships with the gnome-common package. I
      couldn't find an easy way to install gnome-common under windows. So, I
      decided to distribute it. Other ideas?
    
    * Changed default camera index from -1 to 0. For some reason -1 crashes
      mousetrap on Windows. Unless we have a known, verified usecase for -1,
      we should probably change this to 0 in the mainline too (assuming it
      works Linux too).

---
+++ README.windows.md
@@ -0,0 +1,24 @@
+# Installing MouseTrap on Windows
+
+## Prerequisites
+
+* git
+* Common GNU development tools: make, gettext, autotools (installing mingw/msys
+  will do the trick)
+* Python 2.7
+  * Make sure python is in your path for msys
+* setuptools
+* Numpy (needed by OpenCV)
+* OpenCV 2.x (32-bit/x86 - even if you have a 64-bit)
+* PyYAML
+* From PyGObject
+  * core
+  * GTK 3
+  * GDK Pixbuf
+* XLib
+
+## Steps
+1. Install prerequisites using defaults whenever posible
+2. clone project
+3. Follow directions in README.md
+
--- src/mousetrap/i18n.py
+++ src/mousetrap/i18n.py
@@ -5,16 +5,24 @@ from __future__ import division
 import gettext
 import os
 
-import logging
-LOGGER = logging.getLogger(__name__)
+def _(string):
+    return string
 
-LOCALE_DIR = os.path.abspath(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), "locale"))
-LOGGER.debug("LOCALE_DIR = %s", LOCALE_DIR)
-
-translations = gettext.translation("mousetrap", localedir=LOCALE_DIR)
-
-try:
-    _ = translations.ugettext
-except AttributeError:
-    _ = translations.gettext
+#import logging
+#LOGGER = logging.getLogger(__name__)
+#
+#LOCALE_DIR = os.path.abspath(
+#        os.path.join(os.path.dirname(os.path.realpath(__file__)), "locale"))
+#LOGGER.debug("LOCALE_DIR = %s", LOCALE_DIR)
+#
+#if os.path.exists(LOCALE_DIR):
+#    LOGGER.debug("LOCALE_DIR exists")
+#else:
+#    LOGGER.debug("LOCALE_DIR does not exist")
+#
+#translations = gettext.translation("mousetrap", localedir=LOCALE_DIR)
+#
+#try:
+#    _ = translations.ugettext
+#except AttributeError:
+#    _ = translations.gettext
